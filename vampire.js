57ecd0c0d9914a8076aa8824a63dac9f9803122f
Victor Anogwih <anogwihvictor@gmail.com>
2024-02-27 00:15:33 -0500

    vampire_class

--- vampire.js
+++ vampire.js
@@ -10,23 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      numberOfVampires++;
+      currentVampire = currentVampire.creator;
+    }
+    return numberOfVampires;
   }
+  
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
+  
 
   /** Stretch **/
 
@@ -36,7 +45,22 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    const thisAncestors = new Set();
+    let currentVampire = this;
+    while (currentVampire) {
+      thisAncestors.add(currentVampire);
+      currentVampire = currentVampire.creator;
+    }
 
+    currentVampire = vampire;
+    while (currentVampire) {
+      if (thisAncestors.has(currentVampire)) {
+        return currentVampire;
+      }
+      currentVampire = currentVampire.creator;
+    }
+
+    return null; // No common ancestor found
   }
 }
 
